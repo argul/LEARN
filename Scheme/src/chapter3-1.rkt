37d8a2d34d9c9bd70218e2ab1a7bb65a49bb5327
argul <334145486@qq.com>
2014-11-18 08:34:38 +0800

    add loop check

--- Scheme/src/chapter3-1.rkt
+++ Scheme/src/chapter3-1.rkt
@@ -122,3 +122,11 @@
   (loop x '()))
 
 ;(mystery (list 1 2 3 4))
+
+(define (count-pairs x)
+    (length (inner x '())))
+
+(define (inner x memo-list)
+    (if (and (pair? x) (false? (memq x memo-list)))
+        (inner (car x) (inner (cdr x) (cons x memo-list)))
+        memo-list))
