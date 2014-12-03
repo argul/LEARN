ae9eaaa1d98c66eb0f0d9effb4dba0f594745d6e
yangliu <yangliu@kabaminc.com>
2014-12-03 11:21:51 +0800

    ramp up 2014-12-03

--- Scheme/src/chapter3-2.rkt
+++ Scheme/src/chapter3-2.rkt
@@ -1,26 +1,22 @@
 #lang racket
 
-;(require racket/stream)
-;(stream? ones)
-;(stream-empty? ones)
-;(stream-first ones)
-;(in-stream ones) stream->sequence
-;(stream-empty? empty-stream)
-;(stream 1) == (stream-cons 1 empty-stream)
-;(stream->list stream) => list
-;(stream-ref stream 1)
-;(stream-tail stream 2) : {1, 2, 3, 4, 5} => {3, 4, 5}
+;begin skeleton
+(define (mem-proc proc)
+  (let ((executed false) (result false))
+    (lambda ()
+      (if (not executed)
+          (begin (set! result (proc))
+                 (set! executed true)
+                 result)
+          result))))
 
-;(define test-stream (stream-cons 1 (stream-cons 2 empty-stream)))
-;(stream-length test-stream)
+(define (force proc)
+  (proc))
 
-;(define cons-stream stream-cons) // can not be renamed, it's special procedure.
-;(define car-stream stream-first)
-;(define cdr-stream stream-rest)
-;(define stream-null? stream-empty?)
-
-(define (cons-stream first body ...+)
-  (cons (first (delay body))))
+(define-syntax cons-stream
+  (syntax-rules ()
+    ((cons-stream a b)
+     (cons a (mem-proc (lambda () b))))))
 
 (define (car-stream stream)
   (car stream))
@@ -30,37 +26,69 @@
 
 (define empty-stream '())
 (define stream-null? null?)
+;end skeleton
 
-;begin infrastructure test block
+;begin test skeleton
+;(define test
+  ;(cons-stream 1 (cons-stream 2 empty-stream)))
+;(car-stream test)
+;(cdr-stream test)
+;(car-stream (cdr-stream test))
+;(cdr-stream (cdr-stream test))
+;end test skeleton
 
-;end infrastructure test block
+
+;begin functionality
+(define (stream-ref stream n)
+  (if (= n 0)
+      (car-stream stream)
+      (stream-ref (cdr-stream stream) (- n 1))))
+
+(define (stream-for-each proc stream)
+  (if (stream-null? stream)
+      'done
+      (begin (proc (car-stream stream))
+             (stream-for-each proc (cdr-stream stream)))))
 
 (define (stream-1-oper oper stream)
-  (stream-cons (oper (car-stream stream))
+  (cons-stream (oper (car-stream stream))
                (stream-1-oper oper (cdr-stream stream))))
 
 (define (stream-2-oper oper s1 s2)
-  (stream-cons (oper (car-stream s1) (car-stream s2))
+  (cons-stream (oper (car-stream s1) (car-stream s2))
                (stream-2-oper oper (cdr-stream s1) (cdr-stream s2))))
 
 (define (stream-n-oper oper . streams)
-  (stream-cons (oper (map car-stream streams))
+  (cons-stream (oper (map car-stream streams))
                (stream-1-oper oper (map cdr-stream streams))))
 
 (define (stream-head stream n)
   (if (= n 0)
       empty-stream
-      (stream-cons (car-stream stream) (stream-head (cdr-stream stream) (- n 1)))))
+      (cons-stream (car-stream stream) (stream-head (cdr-stream stream) (- n 1)))))
+
+(define (stream-tail stream n)
+  (if (= n 0)
+      stream
+      (stream-tail (cdr-stream stream) (- n 1))))
 
 (define (display-stream stream)
   (begin (stream-for-each (lambda (x) (begin (display x) (display " "))) stream)
          (display "\n")))
 
-(define ones (stream-cons 1 ones))
-(define integers (stream-cons 1 (stream-2-oper + ones integers)))
+(define (display-head stream n)
+  (display-stream (stream-head stream n)))
 
-;(stream-ref integers 0)
-;(stream-ref (stream-tail integers 2) 0)
+;end functionality
 
-;(display-stream (stream-head integers 10))
-;(display-stream (stream-head (stream-tail integers 2) 10))
+;(define (integers-from n)
+;  (cons-stream n (integers-from (+ n 1))))
+;(define integers (integers-from 1))
+;(display-stream (stream-head integers 20))
+
+;begin recursive stream
+(define ones (cons-stream 1 ones))
+;(display-head ones 10)
+(define integers (cons-stream 1 (stream-2-oper + ones integers)))
+;(display-head integers 10)
+;end recursive stream
